9583d3e20c7002dc6fe10b9b9ce2ccd88a332b07
LeeSeungYong <seungyong.lee@timeblocks.com>
2026-07-02 15:08:57 +0900

    chore(android): versionCode 2로 상향

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "com.windrr.boat"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
