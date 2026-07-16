92aa59cf253a6bf7f3a7dd63c95304b70fe72958
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2026-07-16 11:22:39 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "com.windrr.boat"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
