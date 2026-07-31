2e4dbab2513162c6266618d2834b2d77ba84b25d
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2026-08-01 00:22:49 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.windrr.boat"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.0.1"
+        versionCode = 12
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "VISION_API_KEY", "\"${localProps["VISION_API_KEY"]}\"")
